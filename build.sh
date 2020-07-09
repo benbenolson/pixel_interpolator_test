c8dfb52a0f336855ee11335e705d02fe6b8ab54f
Ben Olson <molson5@vols.utk.edu>
2020-07-08 23:34:22 -0500

    Updating build script

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-gcc -lglfw -lGL -lm main.c -I./deps/include -o main
+cd ${DIR}/deps/cglm
+chmod +x autogen.sh
+./autogen.sh
+./configure --prefix=${DIR}/deps/prefix
+make -j5
+make install
+cd ${DIR}
+
+gcc -lglfw -lGL -lm main.c -I${DIR}/deps/prefix/include -o main
